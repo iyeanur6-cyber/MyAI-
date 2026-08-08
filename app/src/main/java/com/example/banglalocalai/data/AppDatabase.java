903e7f7364c2091ac41033372f3febc49c06af3a
iyeanur6-cyber <iyeanur6@gmail.com>
2026-08-08 19:41:49 +0530

    Create AppDatabase.java

---
+++ app/src/main/java/com/example/banglalocalai/data/AppDatabase.java
@@ -0,0 +1,9 @@
+package com.example.banglalocalai.data;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {ChatEntity.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract ChatDao chatDao();
+}
